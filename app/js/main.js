a2c071d69d8033c07eee3a5a5fb40f31481fb191
Kirill Savenkov <leoxopow@gmail.com>
2015-11-08 20:39:53 +0200

    all-tasks

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,17 @@ $(function(){
 	$('.allTask').bxSlider({
 		controls: false
 	});
+	var slideRange = $('#slider-range');
+	slideRange.slider({
+		range: true,
+		min: 0,
+		max: 500,
+		values: [75, 300],
+		slide: function(event, ui){
+			$('#minRange').val(ui.values[0]);
+			$('#maxRange').val(ui.values[1]);
+		}
+	});
+	$('#minRange').val(slideRange.slider('values', 0));
+	$('#maxRange').val(slideRange.slider('values', 1));
 })
